92d06897bbece3be77d8156fe0ad6910e1202ffe
Yuri <4117755ad63@gmail.com>
2021-06-21 00:07:00 +0300

    add event delegation

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const slides = document.body.querySelectorAll(".slide");
 
-slides.forEach((slide) => {
-  slide.addEventListener("click", () => {
-    clearClasses();
-    slide.classList.add("active");
-  });
-});
-
 function clearClasses() {
   slides.forEach((slide) => {
     slide.classList.remove("active");
   });
 }
+
+document.body.addEventListener("click", function (event) {
+  if (!event.target.closest(".slide")) return;
+  clearClasses();
+  event.target.closest(".slide").classList.add("active");
+});
